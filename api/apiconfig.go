2bce9f5435ee9c9338704adfbeb04098e827e30e
Ian Gulliver <git@flamingcow.io>
2022-03-24 17:21:35 +0000

    Split apart API code

---
+++ api/apiconfig.go
@@ -0,0 +1,45 @@
+package api
+
+import "fmt"
+import "net/http"
+import "sync"
+
+type APIConfig struct {
+	Factory func() (interface{}, error)
+	Update  func(interface{}, interface{}) error
+
+	MayCreate func(interface{}, *http.Request) error
+	MayUpdate func(interface{}, interface{}, *http.Request) error
+	MayDelete func(interface{}, *http.Request) error
+	MayRead   func(interface{}, *http.Request) error
+
+	mu sync.RWMutex
+}
+
+func (conf *APIConfig) validate() error {
+	if conf.Factory == nil {
+		return fmt.Errorf("APIConfig.Factory must be set")
+	}
+
+	if conf.Update == nil {
+		return fmt.Errorf("APIConfig.Update must be set")
+	}
+
+	if conf.MayCreate == nil {
+		return fmt.Errorf("APIConfig.MayCreate must be set")
+	}
+
+	if conf.MayUpdate == nil {
+		return fmt.Errorf("APIConfig.MayUpdate must be set")
+	}
+
+	if conf.MayDelete == nil {
+		return fmt.Errorf("APIConfig.MayDelete must be set")
+	}
+
+	if conf.MayRead == nil {
+		return fmt.Errorf("APIConfig.MayRead must be set")
+	}
+
+	return nil
+}
---
+++ api/sse.go
@@ -0,0 +1,27 @@
+package api
+
+import "encoding/json"
+import "fmt"
+import "net/http"
+
+func writeUpdate(w http.ResponseWriter, obj interface{}) error {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return fmt.Errorf("Failed to encode JSON: %s", err)
+	}
+
+	fmt.Fprintf(w, "event: update\ndata: %s\n\n", data)
+	w.(http.Flusher).Flush()
+
+	return nil
+}
+
+func writeDelete(w http.ResponseWriter) {
+	fmt.Fprintf(w, "event: delete\ndata: {}\n\n")
+	w.(http.Flusher).Flush()
+}
+
+func writeHeartbeat(w http.ResponseWriter) {
+	fmt.Fprintf(w, "event: heartbeat\ndata: {}\n\n")
+	w.(http.Flusher).Flush()
+}
