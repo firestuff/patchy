7b3384d148e78c532b3a40e352df69d14b10293e
Ian Gulliver <git@flamingcow.io>
2022-03-25 04:24:31 +0000

    Validate Metadata field in Register()

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -9,11 +9,11 @@ type Metadata struct {
 }
 
 func GetMetadata(obj any) *Metadata {
-	return getMetadataField(obj).Addr().Interface().(*Metadata)
+	return GetMetadataField(obj).Addr().Interface().(*Metadata)
 }
 
 func ClearMetadata(obj any) {
-	getMetadataField(obj).Set(reflect.ValueOf(Metadata{}))
+	GetMetadataField(obj).Set(reflect.ValueOf(Metadata{}))
 }
 
 func (m *Metadata) GetSafeId() string {
@@ -24,15 +24,8 @@ func (m *Metadata) GetKey(t string) string {
 	return fmt.Sprintf("%s:%s", t, m.GetSafeId())
 }
 
-func getMetadataField(obj any) reflect.Value {
-	v := maybeIndirect(obj)
-
-	m := v.FieldByName("Metadata")
-	if !m.IsValid() {
-		panic(fmt.Sprintf("Metadata field missing in %s", v.Type()))
-	}
-
-	return m
+func GetMetadataField(obj any) reflect.Value {
+	return maybeIndirect(obj).FieldByName("Metadata")
 }
 
 func maybeIndirect(obj any) reflect.Value {
