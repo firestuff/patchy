9a07ea1c5a825022ccb5a5bcd1e3b5494e4eb619
Ian Gulliver <git@flamingcow.io>
2022-03-29 04:58:30 +0000

    Initial Idempotency-Key implementation

--- potency/potency.go
+++ potency/potency.go
@@ -1,21 +1,131 @@
 package potency
 
+import "crypto/sha256"
+import "encoding/hex"
+import "fmt"
+import "io"
 import "net/http"
+import "strings"
+import "sync"
 
+import "github.com/firestuff/patchy/metadata"
 import "github.com/firestuff/patchy/store"
 
 type Potency struct {
 	store *store.Store
+
+	inProgress map[string]bool
+	mu         sync.Mutex
+}
+
+type savedResult struct {
+	metadata.Metadata
+
+	Method string `json:"method"`
+	Sha256 string `json:"sha256"`
+
+	StatusCode int         `json:"statusCode"`
+	Header     http.Header `json:"header"`
+	Result     []byte      `json:"result"`
 }
 
 func NewPotency(store *store.Store) *Potency {
 	return &Potency{
-		store: store,
+		store:      store,
+		inProgress: map[string]bool{},
 	}
 }
 
 func (p *Potency) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		val := r.Header.Get("Idempotency-Key")
+		if len(val) < 2 || !strings.HasPrefix(val, `"`) || !strings.HasSuffix(val, `"`) {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		key := val[1 : len(val)-1]
+
+		saved := &savedResult{
+			Metadata: metadata.Metadata{
+				Id: key,
+			},
+		}
+
+		err := p.store.Read("idempotency-key", saved)
+		if err == nil {
+			if r.Method != saved.Method {
+				http.Error(w, "Idempotency-Key method mismatch", http.StatusBadRequest)
+				return
+			}
+
+			h := sha256.New()
+			_, err = io.Copy(h, r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if hex.EncodeToString(h.Sum(nil)) != saved.Sha256 {
+				http.Error(w, "Idempotency-Key request body mismatch", http.StatusUnprocessableEntity)
+				return
+			}
+
+			// TODO: Ability to verify specified headers match (e.g. authentication token) before returning
+
+			w.WriteHeader(saved.StatusCode)
+			w.Write(saved.Result)
+			return
+		}
+
+		// Store miss, proceed to normal execution with interception
+		err = p.lockKey(key)
+		if err != nil {
+			http.Error(w, "Conflict", http.StatusConflict)
+			return
+		}
+		defer p.unlockKey(key)
+
+		bi := newBodyIntercept(r.Body)
+		r.Body = bi
+
+		rwi := newResponseWriterIntercept(w)
+		w = rwi
+
 		next.ServeHTTP(w, r)
+
+		save := &savedResult{
+			Metadata: metadata.Metadata{
+				Id: key,
+			},
+
+			Method: r.Method,
+			Sha256: hex.EncodeToString(bi.sha256.Sum(nil)),
+
+			StatusCode: rwi.statusCode,
+			Header:     rwi.Header(),
+			Result:     rwi.buf.Bytes(),
+		}
+
+		p.store.Write("idempotency-key", save)
 	})
+}
+
+func (p *Potency) lockKey(key string) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if p.inProgress[key] {
+		return fmt.Errorf("Conflict")
+	}
+
+	p.inProgress[key] = true
+
+	return nil
+}
+
+func (p *Potency) unlockKey(key string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	delete(p.inProgress, key)
 }
