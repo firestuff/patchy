6103f6d74e3547122a6e849053575e9678bdfc64
Ian Gulliver <git@flamingcow.io>
2022-04-10 00:35:56 +0000

    Split out list() to enable stream frontend

--- getlist.go
+++ getlist.go
@@ -3,25 +3,13 @@ package api
 import "net/http"
 
 func (api *API) getList(cfg *config, w http.ResponseWriter, r *http.Request) {
-	list, err := api.sb.List(cfg.typeName, cfg.factory)
+	list, err := api.list(cfg, r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	ret := []any{}
-
-	for _, obj := range list {
-		if cfg.mayRead != nil {
-			if cfg.mayRead(obj, r) != nil {
-				continue
-			}
-		}
-
-		ret = append(ret, obj)
-	}
-
-	err = writeJsonList(w, ret)
+	err = writeJsonList(w, list)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
