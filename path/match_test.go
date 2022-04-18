d0401bce930482ae478584452c13a7e4a972eaed
Ian Gulliver <git@flamingcow.io>
2022-04-18 15:14:39 +0000

    Split up tests

--- path/match_test.go
+++ path/match_test.go
@@ -5,7 +5,25 @@ import "time"
 
 import "github.com/stretchr/testify/require"
 
-func TestMatch(t *testing.T) {
+// TODO: Test struct pointers
+// TODO: Negative tests for everything
+// TODO: Better float32 & int comparison
+
+func TestMatchStruct(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType2{
+		Tt1: testType1{
+			Int: 2345,
+		},
+	}, "tt1.int", "2345")
+	require.Nil(t, err)
+	require.True(t, match)
+}
+
+func TestMatchInt(t *testing.T) {
+	t.Parallel()
+
 	match, err := Match(&testType1{
 		Int: 1234,
 	}, "int", "1234")
@@ -17,28 +35,32 @@ func TestMatch(t *testing.T) {
 	}, "int", "1235")
 	require.Nil(t, err)
 	require.False(t, match)
+}
 
-	match, err = Match(&testType2{
-		Tt1: testType1{
-			Int: 2345,
-		},
-	}, "tt1.int", "2345")
-	require.Nil(t, err)
-	require.True(t, match)
+func TestMatchInt64(t *testing.T) {
+	t.Parallel()
 
-	match, err = Match(&testType1{
+	match, err := Match(&testType1{
 		Int64: 3456,
 	}, "int64", "3456")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchUInt(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		UInt: 4567,
 	}, "uint", "4567")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchUInt64(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		UInt64: 5678,
 	}, "uint64", "5678")
 	require.Nil(t, err)
@@ -46,11 +68,15 @@ func TestMatch(t *testing.T) {
 
 	match, err = Match(&testType1{
 		UInt64: 5678,
-	}, "uint64", "5678")
+	}, "uint64", "5679")
 	require.Nil(t, err)
-	require.True(t, match)
+	require.False(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchFloat32(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		Float32: 3.1415,
 	}, "float32", "3.1415")
 	require.Nil(t, err)
@@ -61,14 +87,22 @@ func TestMatch(t *testing.T) {
 	}, "float32", "3.1416")
 	require.Nil(t, err)
 	require.False(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchFloat64(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		Float64: 3.14159265,
 	}, "float64", "3.14159265")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchString(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		String: "foo",
 	}, "string", "foo")
 	require.Nil(t, err)
@@ -76,53 +110,75 @@ func TestMatch(t *testing.T) {
 
 	match, err = Match(&testType1{
 		String: "foo",
-	}, "string", "foo")
+	}, "string", "bar")
 	require.Nil(t, err)
-	require.True(t, match)
+	require.False(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchBool(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		Bool: true,
 	}, "bool", "true")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
-		Strings: []string{"foo", "bar"},
-	}, "strings", "foo")
-	require.Nil(t, err)
-	require.True(t, match)
+func TestMatchInts(t *testing.T) {
+	t.Parallel()
 
-	match, err = Match(&testType1{
+	match, err := Match(&testType1{
 		Ints: []int{2, 4, 7},
 	}, "ints", "4")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchInt64s(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		Int64s: []int64{2, 4, 7},
 	}, "int64s", "4")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchUInts(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		UInts: []uint{2, 4, 7},
 	}, "uints", "4")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchUInt64s(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		UInt64s: []uint64{2, 4, 7},
 	}, "uint64s", "4")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchFloat32s(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		Float32s: []float32{3.1415, 2.7182},
 	}, "float32s", "2.7182")
 	require.Nil(t, err)
 	require.True(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchFloat64s(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		Float64s: []float64{3.1415, 2.7182},
 	}, "float64s", "2.7182")
 	require.Nil(t, err)
@@ -133,14 +189,28 @@ func TestMatch(t *testing.T) {
 	}, "float64s", "2.7183")
 	require.Nil(t, err)
 	require.False(t, match)
+}
+
+func TestMatchStrings(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
+		Strings: []string{"foo", "bar"},
+	}, "strings", "foo")
+	require.Nil(t, err)
+	require.True(t, match)
 
 	match, err = Match(&testType1{
 		Strings: []string{"foo", "bar"},
 	}, "strings", "zig")
 	require.Nil(t, err)
 	require.False(t, match)
+}
 
-	match, err = Match(&testType1{
+func TestMatchBools(t *testing.T) {
+	t.Parallel()
+
+	match, err := Match(&testType1{
 		Bools: []bool{true, false},
 	}, "bools", "true")
 	require.Nil(t, err)
@@ -151,11 +221,15 @@ func TestMatch(t *testing.T) {
 	}, "bools", "true")
 	require.Nil(t, err)
 	require.False(t, match)
+}
+
+func TestMatchTime(t *testing.T) {
+	t.Parallel()
 
 	tm, err := time.Parse("2006-01-02T15:04:05Z", "2006-01-02T15:04:05Z")
 	require.Nil(t, err)
 
-	match, err = Match(&testType1{
+	match, err := Match(&testType1{
 		Time: tm,
 	}, "time", "2006-01-02T15:04:05Z")
 	require.Nil(t, err)
@@ -196,11 +270,18 @@ func TestMatch(t *testing.T) {
 	}, "time", "1136214245001")
 	require.Nil(t, err)
 	require.False(t, match)
+}
+
+func TestMatchTimes(t *testing.T) {
+	t.Parallel()
+
+	tm, err := time.Parse("2006-01-02T15:04:05Z", "2006-01-02T15:04:05Z")
+	require.Nil(t, err)
 
 	tm2, err := time.Parse("2006-01-02T15:04:05Z", "2006-01-10T15:04:05Z")
 	require.Nil(t, err)
 
-	match, err = Match(&testType1{
+	match, err := Match(&testType1{
 		Times: []time.Time{tm, tm2},
 	}, "times", "1136214245000")
 	require.Nil(t, err)
