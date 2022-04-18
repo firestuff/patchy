b15d38718cd80a3c5d014aad66e79aafac5c00fe
Ian Gulliver <git@flamingcow.io>
2022-04-18 03:31:34 +0000

    Remaining basic slice types

--- path/path.go
+++ path/path.go
@@ -49,18 +49,14 @@ func Match(obj any, path string, val1 string) (bool, error) {
 		if err != nil {
 			return false, fmt.Errorf("%s: %w", path, err)
 		}
-		larger := float32(math.Max(math.Abs(v1), math.Abs(float64(v2))))
-		epsilon := math.Nextafter32(larger, math.MaxFloat32) - larger
-		return math.Abs(v1-float64(v2)) < float64(epsilon), nil
+		return float32Equal(v1, float64(v2)), nil
 
 	case float64:
 		v1, err := strconv.ParseFloat(val1, 64)
 		if err != nil {
 			return false, fmt.Errorf("%s: %w", path, err)
 		}
-		larger := math.Max(math.Abs(v1), math.Abs(v2))
-		epsilon := math.Nextafter(larger, math.MaxFloat64) - larger
-		return math.Abs(v1-v2) < epsilon, nil
+		return float64Equal(v1, v2), nil
 
 	case string:
 		return val1 == v2, nil
@@ -71,9 +67,6 @@ func Match(obj any, path string, val1 string) (bool, error) {
 			return false, fmt.Errorf("%s: %w", path, err)
 		}
 		return v1 == v2, nil
-
-	case []string:
-		return slices.Contains(v2, val1), nil
 
 	case []int:
 		v1, err := strconv.ParseInt(val1, 10, 64)
@@ -89,32 +82,78 @@ func Match(obj any, path string, val1 string) (bool, error) {
 		}
 		return slices.Contains(v2, v1), nil
 
-	case time.Time:
-		for _, layout := range []string{
-			"2006-01-02T15:04:05Z",
-			"2006-01-02T15:04:05-07:00",
-		} {
-			v1, err := time.Parse(layout, val1)
-			if err != nil {
-				continue
-			}
-
-			return v1.Equal(v2), nil
-		}
-
-		v1, err := strconv.ParseInt(val1, 10, 64)
+	case []uint:
+		v1, err := strconv.ParseUint(val1, 10, 64)
 		if err != nil {
-			return false, fmt.Errorf("%s: unknown time format", path)
+			return false, fmt.Errorf("%s: %w", path, err)
+		}
+		return slices.Contains(v2, uint(v1)), nil
+
+	case []uint64:
+		v1, err := strconv.ParseUint(val1, 10, 64)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", path, err)
+		}
+		return slices.Contains(v2, v1), nil
+
+	case []float32:
+		v1, err := strconv.ParseFloat(val1, 64)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", path, err)
 		}
 
-		// UNIX Seconds: 2969-05-03
-		// UNIX Millis:  1971-01-01
-		// Intended to give us a wide range of useful values in both schemes
-		if v1 > 31536000000 {
-			return v2.Equal(time.UnixMilli(v1)), nil
-		} else {
-			return v2.Equal(time.Unix(v1, 0)), nil
+		for _, iter := range v2 {
+			if float32Equal(v1, float64(iter)) {
+				return true, nil
+			}
 		}
+
+		return false, nil
+
+	case []float64:
+		v1, err := strconv.ParseFloat(val1, 64)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", path, err)
+		}
+
+		for _, iter := range v2 {
+			if float64Equal(v1, iter) {
+				return true, nil
+			}
+		}
+
+		return false, nil
+
+	case []string:
+		return slices.Contains(v2, val1), nil
+
+	case []bool:
+		v1, err := strconv.ParseBool(val1)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", path, err)
+		}
+		return slices.Contains(v2, v1), nil
+
+	case time.Time:
+		v1, err := parseTime(val1)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", path, err)
+		}
+		return v1.Equal(v2), nil
+
+	case []time.Time:
+		v1, err := parseTime(val1)
+		if err != nil {
+			return false, fmt.Errorf("%s: %w", path, err)
+		}
+
+		for _, iter := range v2 {
+			if v1.Equal(iter) {
+				return true, nil
+			}
+		}
+
+		return false, nil
 
 	default:
 		return false, fmt.Errorf("%s: unsupported struct type (%T)", path, val2)
@@ -160,6 +199,45 @@ func getAnyRecursive(v reflect.Value, parts []string, prev []string) (any, error
 	newPrev = append(newPrev, part)
 
 	return getAnyRecursive(sub, parts[1:], newPrev)
+}
+
+func parseTime(str string) (time.Time, error) {
+	for _, layout := range []string{
+		"2006-01-02T15:04:05Z",
+		"2006-01-02T15:04:05-07:00",
+	} {
+		tm, err := time.Parse(layout, str)
+		if err != nil {
+			continue
+		}
+		return tm, nil
+	}
+
+	i, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("unknown time format")
+	}
+
+	// UNIX Seconds: 2969-05-03
+	// UNIX Millis:  1971-01-01
+	// Intended to give us a wide range of useful values in both schemes
+	if i > 31536000000 {
+		return time.UnixMilli(i), nil
+	} else {
+		return time.Unix(i, 0), nil
+	}
+}
+
+func float32Equal(f1 float64, f2 float64) bool {
+	larger := float32(math.Max(math.Abs(f1), math.Abs(f2)))
+	epsilon := math.Nextafter32(larger, math.MaxFloat32) - larger
+	return math.Abs(f1-f2) < float64(epsilon)
+}
+
+func float64Equal(f1 float64, f2 float64) bool {
+	larger := math.Max(math.Abs(f1), math.Abs(f2))
+	epsilon := math.Nextafter(larger, math.MaxFloat64) - larger
+	return math.Abs(f1-f2) < epsilon
 }
 
 func errorPath(prev []string, part string) string {
