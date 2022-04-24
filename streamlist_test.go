b7a298790dee9bfcc63386e08605430165943775
Ian Gulliver <git@flamingcow.io>
2022-04-24 03:54:30 +0000

    Start of _stream=diff support

--- streamlist_test.go
+++ streamlist_test.go
@@ -125,3 +125,58 @@ func TestStreamList(t *testing.T) {
 		require.True(t, list[0].Text == "foo" || list[0].Text == "bar")
 	})
 }
+
+func TestStreamListDiff(t *testing.T) {
+	t.Parallel()
+
+	withAPI(t, func(t *testing.T, api *API, baseURL string, c *resty.Client) {
+		created1 := &testType{}
+
+		resp, err := c.R().
+			SetBody(&testType{
+				Text: "foo",
+			}).
+			SetResult(created1).
+			Post(fmt.Sprintf("%s/testtype", baseURL))
+		require.Nil(t, err)
+		require.False(t, resp.IsError())
+
+		created2 := &testType{}
+
+		resp, err = c.R().
+			SetBody(&testType{
+				Text: "bar",
+			}).
+			SetResult(created2).
+			Post(fmt.Sprintf("%s/testtype", baseURL))
+		require.Nil(t, err)
+		require.False(t, resp.IsError())
+
+		resp, err = c.R().
+			SetDoNotParseResponse(true).
+			SetHeader("Accept", "text/event-stream").
+			SetQueryParam("_stream", "diff").
+			Get(fmt.Sprintf("%s/testtype", baseURL))
+		require.Nil(t, err)
+		require.False(t, resp.IsError())
+
+		body := resp.RawBody()
+		defer body.Close()
+
+		scan := bufio.NewScanner(body)
+
+		obj1 := testType{}
+
+		eventType, err := readEvent(scan, &obj1)
+		require.Nil(t, err)
+		require.Equal(t, "add", eventType)
+
+		obj2 := testType{}
+
+		eventType, err = readEvent(scan, &obj2)
+		require.Nil(t, err)
+		require.Equal(t, "add", eventType)
+
+		require.ElementsMatch(t, []string{"foo", "bar"}, []string{obj1.Text, obj2.Text})
+	})
+}
