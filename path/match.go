ae8b8d5bdf8ca70ce33ae2aa9d318cb6a6bba91d
Ian Gulliver <git@flamingcow.io>
2022-04-26 05:50:55 +0000

    Split parsing and matching (to enable other comparison operators)

--- path/match.go
+++ path/match.go
@@ -6,141 +6,74 @@ import "time"
 
 import "golang.org/x/exp/slices"
 
-func match(val1 string, val2 any) (bool, error) {
-	switch v2 := val2.(type) {
+type timeMatch struct {
+	time      time.Time
+	precision time.Duration
+}
+
+func match(str string, val any) (bool, error) {
+	s, err := parse(str, val)
+	if err != nil {
+		return false, err
+	}
+
+	switch v := val.(type) {
 	case int:
-		v1, err := strconv.ParseInt(val1, 10, strconv.IntSize)
-		if err != nil {
-			return false, err
-		}
-		return int(v1) == v2, nil
+		return s.(int) == v, nil
 
 	case int64:
-		v1, err := strconv.ParseInt(val1, 10, 64)
-		if err != nil {
-			return false, err
-		}
-		return v1 == v2, nil
+		return s.(int64) == v, nil
 
 	case uint:
-		v1, err := strconv.ParseUint(val1, 10, strconv.IntSize)
-		if err != nil {
-			return false, err
-		}
-		return uint(v1) == v2, nil
+		return s.(uint) == v, nil
 
 	case uint64:
-		v1, err := strconv.ParseUint(val1, 10, 64)
-		if err != nil {
-			return false, err
-		}
-		return v1 == v2, nil
+		return s.(uint64) == v, nil
 
 	case float32:
-		v1, err := strconv.ParseFloat(val1, 32)
-		if err != nil {
-			return false, err
-		}
-		return float32(v1) == v2, nil
+		return s.(float32) == v, nil
 
 	case float64:
-		v1, err := strconv.ParseFloat(val1, 64)
-		if err != nil {
-			return false, err
-		}
-		return v1 == v2, nil
+		return s.(float64) == v, nil
 
 	case string:
-		return val1 == v2, nil
+		return s.(string) == v, nil
 
 	case bool:
-		v1, err := strconv.ParseBool(val1)
-		if err != nil {
-			return false, err
-		}
-		return v1 == v2, nil
+		return s.(bool) == v, nil
 
 	case []int:
-		v1, err := strconv.ParseInt(val1, 10, strconv.IntSize)
-		if err != nil {
-			return false, err
-		}
-		return slices.Contains(v2, int(v1)), nil
+		return slices.Contains(v, s.(int)), nil
 
 	case []int64:
-		v1, err := strconv.ParseInt(val1, 10, 64)
-		if err != nil {
-			return false, err
-		}
-		return slices.Contains(v2, v1), nil
+		return slices.Contains(v, s.(int64)), nil
 
 	case []uint:
-		v1, err := strconv.ParseUint(val1, 10, strconv.IntSize)
-		if err != nil {
-			return false, err
-		}
-		return slices.Contains(v2, uint(v1)), nil
+		return slices.Contains(v, s.(uint)), nil
 
 	case []uint64:
-		v1, err := strconv.ParseUint(val1, 10, 64)
-		if err != nil {
-			return false, err
-		}
-		return slices.Contains(v2, v1), nil
+		return slices.Contains(v, s.(uint64)), nil
 
 	case []float32:
-		v1, err := strconv.ParseFloat(val1, 32)
-		if err != nil {
-			return false, err
-		}
-
-		for _, iter := range v2 {
-			if float32(v1) == iter {
-				return true, nil
-			}
-		}
-
-		return false, nil
+		return slices.Contains(v, s.(float32)), nil
 
 	case []float64:
-		v1, err := strconv.ParseFloat(val1, 64)
-		if err != nil {
-			return false, err
-		}
-
-		for _, iter := range v2 {
-			if v1 == iter {
-				return true, nil
-			}
-		}
-
-		return false, nil
+		return slices.Contains(v, s.(float64)), nil
 
 	case []string:
-		return slices.Contains(v2, val1), nil
+		return slices.Contains(v, s.(string)), nil
 
 	case []bool:
-		v1, err := strconv.ParseBool(val1)
-		if err != nil {
-			return false, err
-		}
-		return slices.Contains(v2, v1), nil
+		return slices.Contains(v, s.(bool)), nil
 
 	case time.Time:
-		v1, precision, err := parseTime(val1)
-		if err != nil {
-			return false, err
-		}
-		return v1.Equal(v2.Truncate(precision)), nil
+		tm := s.(*timeMatch)
+		return tm.time.Equal(v.Truncate(tm.precision)), nil
 
 	case []time.Time:
-		v1, precision, err := parseTime(val1)
-		if err != nil {
-			return false, err
-		}
-
-		for _, iter := range v2 {
-			if v1.Equal(iter.Truncate(precision)) {
+		tm := s.(*timeMatch)
+		for _, iter := range v {
+			if tm.time.Equal(iter.Truncate(tm.precision)) {
 				return true, nil
 			}
 		}
@@ -148,12 +81,87 @@ func match(val1 string, val2 any) (bool, error) {
 		return false, nil
 
 	default:
-		return false, fmt.Errorf("unsupported struct type (%T)", val2)
+		return false, fmt.Errorf("unsupported struct type (%T)", v)
 	}
-
 }
 
-func parseTime(str string) (time.Time, time.Duration, error) {
+func parse(str string, t any) (any, error) {
+	switch v := t.(type) {
+	case int:
+		return parseInt(str)
+
+	case []int:
+		return parseInt(str)
+
+	case int64:
+		return strconv.ParseInt(str, 10, 64)
+
+	case []int64:
+		return strconv.ParseInt(str, 10, 64)
+
+	case uint:
+		return parseUint(str)
+
+	case []uint:
+		return parseUint(str)
+
+	case uint64:
+		return strconv.ParseUint(str, 10, 64)
+
+	case []uint64:
+		return strconv.ParseUint(str, 10, 64)
+
+	case float32:
+		return parseFloat32(str)
+
+	case []float32:
+		return parseFloat32(str)
+
+	case float64:
+		return strconv.ParseFloat(str, 64)
+
+	case []float64:
+		return strconv.ParseFloat(str, 64)
+
+	case string:
+		return str, nil
+
+	case []string:
+		return str, nil
+
+	case bool:
+		return strconv.ParseBool(str)
+
+	case []bool:
+		return strconv.ParseBool(str)
+
+	case time.Time:
+		return parseTime(str)
+
+	case []time.Time:
+		return parseTime(str)
+
+	default:
+		return nil, fmt.Errorf("unsupported struct type (%T)", v)
+	}
+}
+
+func parseInt(str string) (int, error) {
+	val, err := strconv.ParseInt(str, 10, strconv.IntSize)
+	return int(val), err
+}
+
+func parseUint(str string) (uint, error) {
+	val, err := strconv.ParseUint(str, 10, strconv.IntSize)
+	return uint(val), err
+}
+
+func parseFloat32(str string) (float32, error) {
+	val, err := strconv.ParseFloat(str, 32)
+	return float32(val), err
+}
+
+func parseTime(str string) (*timeMatch, error) {
 	for _, layout := range []string{
 		"2006-01-02T15:04:05Z",
 		"2006-01-02T15:04:05-07:00",
@@ -162,20 +170,29 @@ func parseTime(str string) (time.Time, time.Duration, error) {
 		if err != nil {
 			continue
 		}
-		return tm, 1 * time.Second, nil
+		return &timeMatch{
+			time:      tm,
+			precision: 1 * time.Second,
+		}, nil
 	}
 
 	i, err := strconv.ParseInt(str, 10, 64)
 	if err != nil {
-		return time.Time{}, 0, fmt.Errorf("unknown time format")
+		return nil, fmt.Errorf("unknown time format")
 	}
 
 	// UNIX Seconds: 2969-05-03
 	// UNIX Millis:  1971-01-01
 	// Intended to give us a wide range of useful values in both schemes
 	if i > 31536000000 {
-		return time.UnixMilli(i), 1 * time.Millisecond, nil
+		return &timeMatch{
+			time:      time.UnixMilli(i),
+			precision: 1 * time.Millisecond,
+		}, nil
 	} else {
-		return time.Unix(i, 0), 1 * time.Second, nil
+		return &timeMatch{
+			time:      time.Unix(i, 0),
+			precision: 1 * time.Second,
+		}, nil
 	}
 }
