de93e8140104258d8fa096ecfa037352f7365dfd
Ian Gulliver <git@flamingcow.io>
2022-04-29 04:46:36 +0000

    Factor out common operation wrapper

--- path/greater.go
+++ path/greater.go
@@ -5,31 +5,10 @@ import "time"
 import "cloud.google.com/go/civil"
 
 func Greater(obj any, path string, v1Str string) (bool, error) {
-	v2, err := getAny(obj, path)
-	if err != nil {
-		return false, err
-	}
-	if v2 == nil {
-		return false, nil
-	}
-
-	v1, err := parse(v1Str, v2)
-	if err != nil {
-		return false, err
-	}
-
-	return greater(v1, v2), nil
+	return op(obj, path, v1Str, greater)
 }
 
 func greater(v1, v2 any) bool {
-	if v2 == nil {
-		return false
-	}
-
-	if isSlice(v2) {
-		return anyTrue(v2, func(x any) bool { return greater(v1, x) })
-	}
-
 	switch v2t := v2.(type) {
 	case int:
 		return v2t > v1.(int)
---
+++ path/op.go
@@ -0,0 +1,28 @@
+package path
+
+func op(obj any, path string, v1Str string, cb func(any, any) bool) (bool, error) {
+	v2, err := getAny(obj, path)
+	if err != nil {
+		return false, err
+	}
+	if v2 == nil {
+		return false, nil
+	}
+
+	v1, err := parse(v1Str, v2)
+	if err != nil {
+		return false, err
+	}
+
+	if isSlice(v2) {
+		return anyTrue(v2, func(x any) bool {
+			if x == nil {
+				return false
+			}
+
+			return cb(v1, x)
+		}), nil
+	}
+
+	return cb(v1, v2), nil
+}
