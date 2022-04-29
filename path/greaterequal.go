46a564061f80f254cf951289257a092bef7d37d7
Ian Gulliver <git@flamingcow.io>
2022-04-29 04:36:35 +0000

    Switch to helpers

--- path/greaterequal.go
+++ path/greaterequal.go
@@ -1,128 +1,69 @@
 package path
 
-import "fmt"
 import "time"
 
 import "cloud.google.com/go/civil"
-import "golang.org/x/exp/constraints"
 
-func GreaterEqual(obj any, path string, val1 string) (bool, error) {
-	val2, err := getAny(obj, path)
+func GreaterEqual(obj any, path string, v1Str string) (bool, error) {
+	v2, err := getAny(obj, path)
 	if err != nil {
 		return false, err
 	}
-
-	if val2 == nil {
+	if v2 == nil {
 		return false, nil
 	}
 
-	ret, err := greaterEqual(val1, val2)
-	if err != nil {
-		return false, fmt.Errorf("%s: %w", path, err)
-	}
-
-	return ret, nil
-}
-
-func greaterEqual(str string, val any) (bool, error) {
-	s, err := parse(str, val)
+	v1, err := parse(v1Str, v2)
 	if err != nil {
 		return false, err
 	}
 
-	switch v := val.(type) {
+	return greaterEqual(v1, v2), nil
+}
+
+func greaterEqual(v1, v2 any) bool {
+	if v2 == nil {
+		return false
+	}
+
+	if isSlice(v2) {
+		return anyTrue(v2, func(x any) bool { return greaterEqual(v1, x) })
+	}
+
+	switch v2t := v2.(type) {
 	case int:
-		return v >= s.(int), nil
+		return v2t >= v1.(int)
 
 	case int64:
-		return v >= s.(int64), nil
+		return v2t >= v1.(int64)
 
 	case uint:
-		return v >= s.(uint), nil
+		return v2t >= v1.(uint)
 
 	case uint64:
-		return v >= s.(uint64), nil
+		return v2t >= v1.(uint64)
 
 	case float32:
-		return v >= s.(float32), nil
+		return v2t >= v1.(float32)
 
 	case float64:
-		return v >= s.(float64), nil
+		return v2t >= v1.(float64)
 
 	case string:
-		return v >= s.(string), nil
+		return v2t >= v1.(string)
 
 	case bool:
-		return v == true || v == s.(bool), nil
-
-	case []int:
-		return containsGreaterEqual(v, s.(int)), nil
-
-	case []int64:
-		return containsGreaterEqual(v, s.(int64)), nil
-
-	case []uint:
-		return containsGreaterEqual(v, s.(uint)), nil
-
-	case []uint64:
-		return containsGreaterEqual(v, s.(uint64)), nil
-
-	case []float32:
-		return containsGreaterEqual(v, s.(float32)), nil
-
-	case []float64:
-		return containsGreaterEqual(v, s.(float64)), nil
-
-	case []string:
-		return containsGreaterEqual(v, s.(string)), nil
-
-	case []bool:
-		for _, iter := range v {
-			if iter == true || iter == s.(bool) {
-				return true, nil
-			}
-		}
-
-		return false, nil
+		return v2t == true || v2t == v1.(bool)
 
 	case time.Time:
-		tm := s.(*timeVal)
-		trunc := v.Truncate(tm.precision)
-		return trunc.Equal(tm.time) || trunc.After(tm.time), nil
-
-	case []time.Time:
-		tm := s.(*timeVal)
-		for _, iter := range v {
-			trunc := iter.Truncate(tm.precision)
-			if trunc.Equal(tm.time) || trunc.After(tm.time) {
-				return true, nil
-			}
-		}
-
-		return false, nil
+		tm := v1.(*timeVal)
+		trunc := v2t.Truncate(tm.precision)
+		return trunc.Equal(tm.time) || trunc.After(tm.time)
 
 	case civil.Date:
-		return v == s.(civil.Date) || v.After(s.(civil.Date)), nil
-
-	case []civil.Date:
-		for _, iter := range v {
-			if iter == s.(civil.Date) || iter.After(s.(civil.Date)) {
-				return true, nil
-			}
-		}
-
-		return false, nil
+		return v2t == v1.(civil.Date) || v2t.After(v1.(civil.Date))
 
 	default:
-		return false, fmt.Errorf("unsupported struct type (%T)", v)
+		panic(v2)
 	}
-}
-
-func containsGreaterEqual[E constraints.Ordered](s []E, v E) bool {
-	for _, iter := range s {
-		if iter >= v {
-			return true
-		}
-	}
-	return false
 }
