243a3bde2b3f2ebf331426ba586bd92478435962
Ian Gulliver <git@flamingcow.io>
2022-04-30 05:10:04 +0000

    TestNested() skeleton

---
+++ nest_test.go
@@ -0,0 +1,26 @@
+package patchy_test
+
+import (
+	"testing"
+
+	"github.com/firestuff/patchy"
+	"github.com/go-resty/resty/v2"
+)
+
+type Outer struct {
+	Text  string
+	Inner []string
+}
+
+type Inner struct {
+	Text string
+}
+
+func TestNested(t *testing.T) {
+	t.Parallel()
+
+	withAPI(t, func(t *testing.T, api *patchy.API, baseURL string, c *resty.Client) {
+		patchy.Register[Outer](api)
+		patchy.Register[Inner](api)
+	})
+}
