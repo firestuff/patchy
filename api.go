8344453878c2c57b88a18dda2dadd7090e7bb18c
Ian Gulliver <git@flamingcow.io>
2022-04-30 18:41:51 +0000

    Split up RegisterName()

--- api.go
+++ api.go
@@ -37,26 +37,6 @@ func NewAPI(st store.Storer) (*API, error) {
 	return api, nil
 }
 
-type mayCreate interface {
-	MayCreate(*http.Request) error
-}
-
-type mayReplace[T any] interface {
-	MayReplace(*T, *http.Request) error
-}
-
-type mayUpdate[T any] interface {
-	MayUpdate(*T, *http.Request) error
-}
-
-type mayDelete interface {
-	MayDelete(*http.Request) error
-}
-
-type mayRead interface {
-	MayRead(*http.Request) error
-}
-
 func Register[T any](api *API) {
 	obj := new(T)
 	t := reflect.TypeOf(*obj)
@@ -64,101 +44,65 @@ func Register[T any](api *API) {
 }
 
 func RegisterName[T any](api *API, t string) {
-	cfg := &config{
-		typeName: t,
-		factory:  func() any { return new(T) },
-	}
+	cfg := newConfig[T](t)
+	api.registerHandlers(fmt.Sprintf("/%s", t), cfg)
+}
 
-	obj := new(T)
+func (api *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	api.router.ServeHTTP(w, r)
+}
 
-	if !metadata.HasMetadata(obj) {
-		panic("struct missing patchy.Metadata field")
-	}
-
-	if _, has := any(obj).(mayCreate); has {
-		cfg.mayCreate = func(obj any, r *http.Request) error {
-			return obj.(mayCreate).MayCreate(r)
-		}
-	}
-
-	if _, found := any(obj).(mayReplace[T]); found {
-		cfg.mayReplace = func(obj any, replace any, r *http.Request) error {
-			return obj.(mayReplace[T]).MayReplace(replace.(*T), r)
-		}
-	}
-
-	if _, found := any(obj).(mayUpdate[T]); found {
-		cfg.mayUpdate = func(obj any, patch any, r *http.Request) error {
-			return obj.(mayUpdate[T]).MayUpdate(patch.(*T), r)
-		}
-	}
-
-	if _, has := any(obj).(mayDelete); has {
-		cfg.mayDelete = func(obj any, r *http.Request) error {
-			return obj.(mayDelete).MayDelete(r)
-		}
-	}
-
-	if _, has := any(obj).(mayRead); has {
-		cfg.mayRead = func(obj any, r *http.Request) error {
-			return obj.(mayRead).MayRead(r)
-		}
-	}
-
+func (api *API) registerHandlers(base string, cfg *config) {
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s", t),
+		base,
 		func(w http.ResponseWriter, r *http.Request) { api.streamList(cfg, w, r) },
 	).
 		Methods("GET").
 		Headers("Accept", "text/event-stream")
 
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s", t),
+		base,
 		func(w http.ResponseWriter, r *http.Request) { api.getList(cfg, w, r) },
 	).
 		Methods("GET")
 
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s", t),
+		base,
 		func(w http.ResponseWriter, r *http.Request) { api.post(cfg, w, r) },
 	).
 		Methods("POST").
 		Headers("Content-Type", "application/json")
 
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s/{id}", t),
+		fmt.Sprintf("%s/{id}", base),
 		func(w http.ResponseWriter, r *http.Request) { api.put(cfg, w, r) },
 	).
 		Methods("PUT").
 		Headers("Content-Type", "application/json")
 
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s/{id}", t),
+		fmt.Sprintf("%s/{id}", base),
 		func(w http.ResponseWriter, r *http.Request) { api.patch(cfg, w, r) },
 	).
 		Methods("PATCH").
 		Headers("Content-Type", "application/json")
 
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s/{id}", t),
+		fmt.Sprintf("%s/{id}", base),
 		func(w http.ResponseWriter, r *http.Request) { api.delete(cfg, w, r) },
 	).
 		Methods("DELETE")
 
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s/{id}", t),
+		fmt.Sprintf("%s/{id}", base),
 		func(w http.ResponseWriter, r *http.Request) { api.stream(cfg, w, r) },
 	).
 		Methods("GET").
 		Headers("Accept", "text/event-stream")
 
 	api.router.HandleFunc(
-		fmt.Sprintf("/%s/{id}", t),
+		fmt.Sprintf("%s/{id}", base),
 		func(w http.ResponseWriter, r *http.Request) { api.get(cfg, w, r) },
 	).
 		Methods("GET")
-}
-
-func (api *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	api.router.ServeHTTP(w, r)
 }
