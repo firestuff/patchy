970d92c5cfd7958e88b1ea20d274fce7b6d6954e
Ian Gulliver <git@flamingcow.io>
2022-11-25 00:16:02 +0000

    Support filters returning errors

--- view/filterview.go
+++ view/filterview.go
@@ -4,7 +4,7 @@ type FilterView[T any] struct {
 	ch chan T
 }
 
-func NewFilterView[T any](input ReadView[T], filter func(T) T) *FilterView[T] {
+func NewFilterView[T any](input ReadView[T], filter func(T) (T, error)) *FilterView[T] {
 	v := &FilterView[T]{
 		ch: make(chan T, 100),
 	}
@@ -13,8 +13,13 @@ func NewFilterView[T any](input ReadView[T], filter func(T) T) *FilterView[T] {
 		defer close(v.ch)
 
 		for update := range input.Chan() {
+			f, err := filter(update)
+			if err != nil {
+				break
+			}
+
 			select {
-			case v.ch <- filter(update):
+			case v.ch <- f:
 
 			default:
 				break
