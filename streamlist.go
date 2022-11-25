b394794e45c748a31a84e5ce170aacc30d009f96
Ian Gulliver <git@flamingcow.io>
2022-11-25 00:05:52 +0000

    Complete initial view conversion

--- streamlist.go
+++ streamlist.go
@@ -36,26 +36,18 @@ func (api *API) streamList(cfg *config, w http.ResponseWriter, r *http.Request) 
 	case "":
 		fallthrough
 	case "full":
-		api.streamListFull(cfg, w, r, params, parsed)
+		api.streamListFull(cfg, w, r, parsed)
 
 	case "diff":
-		api.streamListDiff(cfg, w, r, params, parsed)
+		api.streamListDiff(cfg, w, r, parsed)
 
 	default:
 		http.Error(w, "Unknown _stream format", http.StatusBadRequest)
 	}
 }
 
-func (api *API) streamListFull(cfg *config, w http.ResponseWriter, r *http.Request, params url.Values, parsed *listParams) {
-	updated, deleted := api.sb.SubscribeType(cfg.typeName)
-
-	prev, err := api.list(cfg, r, parsed)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = writeEvent(w, "list", prev)
+func (api *API) streamListFull(cfg *config, w http.ResponseWriter, r *http.Request, params *listParams) {
+	v, err := api.sb.List(r.Context(), cfg.typeName, cfg.factory)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -64,13 +56,9 @@ func (api *API) streamListFull(cfg *config, w http.ResponseWriter, r *http.Reque
 	ticker := time.NewTicker(5 * time.Second)
 
 	for {
-		changedID := ""
-
 		select {
-		case u := <-updated:
-			changedID = metadata.GetMetadata(u).ID
-
-		case <-deleted:
+		case <-r.Context().Done():
+			return
 
 		case <-ticker.C:
 			err = writeEvent(w, "heartbeat", emptyEvent)
@@ -80,61 +68,33 @@ func (api *API) streamListFull(cfg *config, w http.ResponseWriter, r *http.Reque
 
 			continue
 
-		case <-r.Context().Done():
-			return
-		}
+		case list := <-v.Chan():
+			list2, err := filterList(cfg, r, params, list)
+			if err != nil {
+				return
+			}
 
-		list, err := api.list(cfg, r, parsed)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			err = writeEvent(w, "list", list2)
+			if err != nil {
+				return
+			}
 		}
-
-		if listMatches(prev, list, changedID) {
-			continue
-		}
-
-		err = writeEvent(w, "list", list)
-		if err != nil {
-			return
-		}
-
-		prev = list
 	}
 }
 
-func (api *API) streamListDiff(cfg *config, w http.ResponseWriter, r *http.Request, params url.Values, parsed *listParams) {
-	updated, deleted := api.sb.SubscribeType(cfg.typeName)
-
-	list, err := api.list(cfg, r, parsed)
+func (api *API) streamListDiff(cfg *config, w http.ResponseWriter, r *http.Request, params *listParams) {
+	v, err := api.sb.List(r.Context(), cfg.typeName, cfg.factory)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	live := map[string]any{}
-
-	for _, obj := range list {
-		err = writeEvent(w, "add", obj)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		live[metadata.GetMetadata(obj).ID] = obj
-	}
+	last := map[string]any{}
 
 	ticker := time.NewTicker(5 * time.Second)
 
 	for {
-		changedID := ""
-
 		select {
-		case u := <-updated:
-			changedID = metadata.GetMetadata(u).ID
-
-		case <-deleted:
-
 		case <-ticker.C:
 			err = writeEvent(w, "heartbeat", emptyEvent)
 			if err != nil {
@@ -145,76 +105,51 @@ func (api *API) streamListDiff(cfg *config, w http.ResponseWriter, r *http.Reque
 
 		case <-r.Context().Done():
 			return
-		}
 
-		list, err := api.list(cfg, r, parsed)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		case list := <-v.Chan():
+			list2, err := filterList(cfg, r, params, list)
+			if err != nil {
+				return
+			}
 
-		cur := map[string]any{}
+			cur := map[string]any{}
 
-		for _, obj := range list {
-			objMD := metadata.GetMetadata(obj)
+			for _, obj := range list2 {
+				objMD := metadata.GetMetadata(obj)
 
-			_, found := live[objMD.ID]
-			if found {
-				// In both old and new lists, but flagged as changed by subscription chan
-				if objMD.ID == changedID {
-					err = writeEvent(w, "update", obj)
+				lastObj, found := last[objMD.ID]
+				if found {
+					lastMD := metadata.GetMetadata(lastObj)
+					if objMD.ETag != lastMD.ETag {
+						err = writeEvent(w, "update", obj)
+						if err != nil {
+							return
+						}
+					}
+				} else {
+					err = writeEvent(w, "add", obj)
 					if err != nil {
 						return
 					}
 				}
-			} else {
-				// In the new list but not the old
-				err = writeEvent(w, "add", obj)
+
+				cur[objMD.ID] = obj
+				last[objMD.ID] = obj
+			}
+
+			for id, old := range last {
+				_, found := cur[id]
+				if found {
+					continue
+				}
+
+				err = writeEvent(w, "remove", old)
 				if err != nil {
 					return
 				}
+
+				delete(last, id)
 			}
-
-			cur[objMD.ID] = obj
-			live[objMD.ID] = obj
-		}
-
-		for id, old := range live {
-			_, found := cur[id]
-			if found {
-				continue
-			}
-
-			// In the old list but not the new
-			err = writeEvent(w, "remove", old)
-			if err != nil {
-				return
-			}
-
-			delete(live, id)
 		}
 	}
-}
-
-func listMatches(l1 []any, l2 []any, changedID string) bool {
-	if len(l1) != len(l2) {
-		return false
-	}
-
-	for i, o1 := range l1 {
-		o1ID := metadata.GetMetadata(o1).ID
-
-		if changedID != "" && o1ID == changedID {
-			return false
-		}
-
-		o2 := l2[i]
-		o2ID := metadata.GetMetadata(o2).ID
-
-		if o1ID != o2ID {
-			return false
-		}
-	}
-
-	return true
 }
