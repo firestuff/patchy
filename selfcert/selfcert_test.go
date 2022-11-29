aa9e1b32afe9e4811a047797fde508e357deb9a7
Ian Gulliver <git@flamingcow.io>
2022-11-29 04:43:04 +0000

    Add self-signed cert generator

---
+++ selfcert/selfcert_test.go
@@ -0,0 +1,49 @@
+package selfcert_test
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/firestuff/patchy/selfcert"
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCert(t *testing.T) {
+	t.Parallel()
+
+	conf, err := selfcert.NewTLSConfig([]string{"junk"})
+	require.Nil(t, err)
+
+	listener, err := tls.Listen("tcp", "[::]:0", conf)
+	require.Nil(t, err)
+
+	baseURL := fmt.Sprintf("https://[::1]:%d/", listener.Addr().(*net.TCPAddr).Port)
+
+	srv := &http.Server{
+		ReadHeaderTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		_ = srv.Serve(listener)
+	}()
+
+	cli := resty.New()
+	cli.SetBaseURL(baseURL)
+	cli.SetTLSClientConfig(&tls.Config{
+		InsecureSkipVerify: true, //nolint: gosec
+	})
+
+	resp, err := cli.R().Get("/")
+	require.Nil(t, err)
+	require.True(t, resp.IsError())
+	require.Equal(t, resp.StatusCode(), 404)
+
+	err = srv.Shutdown(context.Background())
+	require.Nil(t, err)
+}
