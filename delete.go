77da6f43dc1c3bcd15b04d35f6e948a6895b8282
Ian Gulliver <github@flamingcow.io>
2022-12-31 16:13:17 +0000

    jsrest.Error checkpoint

--- delete.go
+++ delete.go
@@ -1,33 +1,48 @@
 package patchy
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/firestuff/patchy/jsrest"
 )
 
 func (api *API) delete(cfg *config, id string, w http.ResponseWriter, r *http.Request) {
 	v, err := api.sb.Read(r.Context(), cfg.typeName, id, cfg.factory)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		e := fmt.Errorf("failed to read %s: %w", id, err)
+		jse := jsrest.FromError(e, jsrest.StatusInternalServerError)
+		jse.Write(w)
+
 		return
 	}
 
 	obj := <-v.Chan()
 	if obj == nil {
-		http.Error(w, "not found", http.StatusNotFound)
+		e := fmt.Errorf("%s: %w", id, ErrNotFound)
+		jse := jsrest.FromError(e, jsrest.StatusNotFound)
+		jse.Write(w)
+
 		return
 	}
 
 	if cfg.mayDelete != nil {
 		err = cfg.mayDelete(obj, r)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusUnauthorized)
+			e := fmt.Errorf("unauthorized %s: %w", id, err)
+			jse := jsrest.FromError(e, jsrest.StatusUnauthorized)
+			jse.Write(w)
+
 			return
 		}
 	}
 
 	err = api.sb.Delete(cfg.typeName, id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		e := fmt.Errorf("failed to delete %s: %w", id, err)
+		jse := jsrest.FromError(e, jsrest.StatusInternalServerError)
+		jse.Write(w)
+
 		return
 	}
 
--- get.go
+++ get.go
@@ -2,6 +2,7 @@ package patchy
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/firestuff/patchy/jsrest"
@@ -12,21 +13,30 @@ var ErrNotFound = errors.New("not found")
 func (api *API) get(cfg *config, id string, w http.ResponseWriter, r *http.Request) {
 	v, err := api.sb.Read(r.Context(), cfg.typeName, id, cfg.factory)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		e := fmt.Errorf("failed to read %s: %w", id, err)
+		jse := jsrest.FromError(e, jsrest.StatusInternalServerError)
+		jse.Write(w)
+
 		return
 	}
 
 	obj := <-v.Chan()
 
 	if obj == nil {
-		http.Error(w, "not found", http.StatusNotFound)
+		e := fmt.Errorf("%s: %w", id, ErrNotFound)
+		jse := jsrest.FromError(e, jsrest.StatusNotFound)
+		jse.Write(w)
+
 		return
 	}
 
 	if cfg.mayRead != nil {
 		err = cfg.mayRead(obj, r)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusUnauthorized)
+			e := fmt.Errorf("unauthorized %s: %w", id, err)
+			jse := jsrest.FromError(e, jsrest.StatusUnauthorized)
+			jse.Write(w)
+
 			return
 		}
 	}
