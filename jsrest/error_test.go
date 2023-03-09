cc9a5b1f0beef686b51b1b98493a8035b521a8e0
Ian Gulliver <github@flamingcow.io>
2023-03-09 06:13:25 +0000

    Support golang 1.20 multiple error unwrapping

--- jsrest/error_test.go
+++ jsrest/error_test.go
@@ -21,6 +21,19 @@ func TestFromError(t *testing.T) {
 	require.Equal(t, e.Messages, []string{"error 2: error 1", "error 1"})
 }
 
+func TestFromErrors(t *testing.T) {
+	t.Parallel()
+
+	e1 := errors.New("error 1") //nolint:goerr113
+	e2 := errors.New("error 2") //nolint:goerr113
+	e3 := fmt.Errorf("error 3: %w + %w", e1, e2)
+
+	e := jsrest.FromError(e3, jsrest.StatusBadGateway)
+
+	require.Equal(t, e.Code, jsrest.StatusBadGateway)
+	require.Equal(t, e.Messages, []string{"error 3: error 1 + error 2", "error 1", "error 2"})
+}
+
 func TestParams(t *testing.T) {
 	t.Parallel()
 
