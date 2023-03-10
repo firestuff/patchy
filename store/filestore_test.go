95b4a471b98f72a022903714b87c81299c06199e
Ian Gulliver <github@flamingcow.io>
2023-03-10 23:33:32 +0000

    Add SQLiteStore, simplify Filestore

--- store/filestore_test.go
+++ store/filestore_test.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/firestuff/patchy/metadata"
 	"github.com/firestuff/patchy/store"
 	"github.com/stretchr/testify/require"
 )
@@ -17,31 +16,9 @@ func TestFileStore(t *testing.T) {
 
 	defer os.RemoveAll(dir)
 
-	store := store.NewFileStore(dir)
+	st := store.NewFileStore(dir)
 
-	err = store.Write("storeTest", &storeTest{
-		Metadata: metadata.Metadata{
-			ID: "id1",
-		},
-		Opaque: "foo",
-	})
-	require.Nil(t, err)
-
-	err = store.Write("storeTest", &storeTest{
-		Metadata: metadata.Metadata{
-			ID: "id2",
-		},
-		Opaque: "bar",
-	})
-	require.Nil(t, err)
-
-	out1, err := store.Read("storeTest", "id1", newStoreTest)
-	require.Nil(t, err)
-	require.Equal(t, "foo", out1.(*storeTest).Opaque)
-
-	out2, err := store.Read("storeTest", "id2", newStoreTest)
-	require.Nil(t, err)
-	require.Equal(t, "bar", out2.(*storeTest).Opaque)
+	testStorer(t, st)
 }
 
 func TestFileStoreDelete(t *testing.T) {
@@ -52,26 +29,9 @@ func TestFileStoreDelete(t *testing.T) {
 
 	defer os.RemoveAll(dir)
 
-	store := store.NewFileStore(dir)
+	st := store.NewFileStore(dir)
 
-	err = store.Write("storeTest", &storeTest{
-		Metadata: metadata.Metadata{
-			ID: "id1",
-		},
-		Opaque: "foo",
-	})
-	require.Nil(t, err)
-
-	out1, err := store.Read("storeTest", "id1", newStoreTest)
-	require.Nil(t, err)
-	require.Equal(t, "foo", out1.(*storeTest).Opaque)
-
-	err = store.Delete("storeTest", "id1")
-	require.Nil(t, err)
-
-	out2, err := store.Read("storeTest", "id1", newStoreTest)
-	require.Nil(t, err)
-	require.Nil(t, out2)
+	testDelete(t, st)
 }
 
 func TestFileStoreList(t *testing.T) {
@@ -82,39 +42,7 @@ func TestFileStoreList(t *testing.T) {
 
 	defer os.RemoveAll(dir)
 
-	store := store.NewFileStore(dir)
+	st := store.NewFileStore(dir)
 
-	objs, err := store.List("storeTest", func() any { return &storeTest{} })
-	require.Nil(t, err)
-	require.Len(t, objs, 0)
-
-	err = store.Write("storeTest", &storeTest{
-		Metadata: metadata.Metadata{
-			ID: "id1",
-		},
-		Opaque: "foo",
-	})
-	require.Nil(t, err)
-
-	err = store.Write("storeTest", &storeTest{
-		Metadata: metadata.Metadata{
-			ID: "id2",
-		},
-		Opaque: "bar",
-	})
-	require.Nil(t, err)
-
-	objs, err = store.List("storeTest", func() any { return &storeTest{} })
-	require.Nil(t, err)
-	require.Len(t, objs, 2)
-	require.ElementsMatch(t, []string{"foo", "bar"}, []string{objs[0].(*storeTest).Opaque, objs[1].(*storeTest).Opaque})
-}
-
-type storeTest struct {
-	metadata.Metadata
-	Opaque string
-}
-
-func newStoreTest() any {
-	return &storeTest{}
+	testList(t, st)
 }
---
+++ store/sqlitestore.go
@@ -0,0 +1,146 @@
+package store
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/firestuff/patchy/metadata"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type SQLiteStore struct {
+	db *sql.DB
+}
+
+func NewSQLiteStore(conn string) (*SQLiteStore, error) {
+	db, err := sql.Open("sqlite3", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SQLiteStore{
+		db: db,
+	}, nil
+}
+
+func (sls *SQLiteStore) Close() {
+	sls.db.Close()
+}
+
+func (sls *SQLiteStore) Write(t string, obj any) error {
+	id := metadata.GetMetadata(obj).ID
+
+	js, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+
+	_, err = sls.exec("INSERT INTO `%s` (id, obj) VALUES (?,?);", t, id, js)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sls *SQLiteStore) Delete(t string, id string) error {
+	_, err := sls.exec("DELETE FROM `%s` WHERE id=?", t, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sls *SQLiteStore) Read(t string, id string, factory func() any) (any, error) {
+	rows, err := sls.query("SELECT obj FROM `%s` WHERE id=?;", t, id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var js []byte
+
+		err = rows.Scan(&js)
+		if err != nil {
+			return nil, err
+		}
+
+		obj := factory()
+
+		err = json.Unmarshal(js, obj)
+		if err != nil {
+			return nil, err
+		}
+
+		return obj, nil
+	}
+
+	return nil, nil
+}
+
+func (sls *SQLiteStore) List(t string, factory func() any) ([]any, error) {
+	rows, err := sls.query("SELECT obj FROM `%s`;", t)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	ret := []any{}
+
+	for rows.Next() {
+		var js []byte
+
+		err = rows.Scan(&js)
+		if err != nil {
+			return nil, err
+		}
+
+		obj := factory()
+
+		err = json.Unmarshal(js, obj)
+		if err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, obj)
+	}
+
+	return ret, nil
+}
+
+func (sls *SQLiteStore) exec(query, t string, args ...any) (sql.Result, error) {
+	query = fmt.Sprintf(query, t)
+
+	result, err := sls.db.Exec(query, args...)
+	if err == nil {
+		return result, nil
+	}
+
+	_, err = sls.db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS `%s` (id TEXT NOT NULL PRIMARY KEY, obj TEXT NOT NULL);", t))
+	if err != nil {
+		return nil, err
+	}
+
+	return sls.db.Exec(query, args...)
+}
+
+func (sls *SQLiteStore) query(query, t string, args ...any) (*sql.Rows, error) {
+	query = fmt.Sprintf(query, t)
+
+	rows, err := sls.db.Query(query, args...)
+	if err == nil {
+		return rows, nil
+	}
+
+	_, err = sls.db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS `%s` (id TEXT NOT NULL PRIMARY KEY, obj TEXT NOT NULL);", t))
+	if err != nil {
+		return nil, err
+	}
+
+	return sls.db.Query(query, args...)
+}
