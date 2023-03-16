5c9a7bce70d022502f5b4b033c2bcba9049895fe
Ian Gulliver <github@flamingcow.io>
2023-03-16 22:46:17 +0000

    Changes to Is*() signatures

--- api/util.go
+++ api/util.go
@@ -1,13 +1,13 @@
 package api
 
-func IsCreate(obj *any, prev *any) bool {
+func IsCreate(obj any, prev any) bool {
 	return obj != nil && prev == nil
 }
 
-func IsUpdate(obj *any, prev *any) bool {
+func IsUpdate(obj any, prev any) bool {
 	return obj != nil && prev != nil
 }
 
-func IsDelete(obj *any, prev *any) bool {
+func IsDelete(obj any, prev any) bool {
 	return obj == nil && prev != nil
 }
