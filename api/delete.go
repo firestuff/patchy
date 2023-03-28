e1e0213bb093a89c6f641945d22cb8ba82909052
Ian Gulliver <github@flamingcow.io>
2023-03-28 03:12:31 +0000

    Add If-Match/If-None-Match docs

--- api/delete.go
+++ api/delete.go
@@ -7,6 +7,7 @@ import (
 )
 
 func (api *API) delete(cfg *config, id string, w http.ResponseWriter, r *http.Request) error {
+	// TODO: Support If-Match
 	err := api.deleteInt(r.Context(), cfg, id)
 	if err != nil {
 		return jsrest.Errorf(jsrest.ErrInternalServerError, "delete failed (%w)", err)
