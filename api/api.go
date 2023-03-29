c7f655967dbd42e87a32efd32a33612fa977db7f
Ian Gulliver <github@flamingcow.io>
2023-03-29 17:54:59 +0000

    Clarify caching policy

--- api/api.go
+++ api/api.go
@@ -174,7 +174,7 @@ func (api *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: Gate CORs with some kind of flag
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Cache-Control", "no-store")
 
 	api.potency.ServeHTTP(w, r)
 }
