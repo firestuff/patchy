c8e4109c3d0fa1ddb5ba886d9e7b1fdeda3bd80c
Ian Gulliver <github@flamingcow.io>
2023-04-08 14:35:28 +0000

    Handle UpdateOpts in replace*() and update*()

---
+++ api/ts_test/replace_prev_test.ts
@@ -0,0 +1,16 @@
+import { test } from 'node:test';
+import { strict as assert } from 'node:assert';
+import { TestClient } from './util.js';
+
+test('replace success', async () => {
+	const tc = new TestClient();
+
+	const create = await tc.client.createTestType({text: "foo", num: 5});
+	const get1 = await tc.client.getTestType(create.id);
+	await tc.client.replaceTestType(create.id, {text: "bar"});
+
+	assert.rejects(tc.client.replaceTestType(create.id, {text: "zig"}, {prev: get1}));
+
+	const get2 = await tc.client.getTestType(create.id);
+	assert.equal(get2.text, "bar");
+});
---
+++ api/ts_test/update_prev_test.ts
@@ -0,0 +1,16 @@
+import { test } from 'node:test';
+import { strict as assert } from 'node:assert';
+import { TestClient } from './util.js';
+
+test('update success', async () => {
+	const tc = new TestClient();
+
+	const create = await tc.client.createTestType({text: "foo", num: 5});
+	const get1 = await tc.client.getTestType(create.id);
+	await tc.client.updateTestType(create.id, {text: "bar"});
+
+	assert.rejects(tc.client.updateTestType(create.id, {text: "zig"}, {prev: get1}));
+
+	const get2 = await tc.client.getTestType(create.id);
+	assert.equal(get2.text, "bar");
+});
