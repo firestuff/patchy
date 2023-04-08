b19f9fba540bcc4044be454bd11f764d4ee6b0ad
Ian Gulliver <github@flamingcow.io>
2023-04-08 14:01:00 +0000

    Add UpdateOpts to delete*()

---
+++ api/ts_test/delete_prev_test.ts
@@ -0,0 +1,13 @@
+import { test } from 'node:test';
+import { strict as assert } from 'node:assert';
+import { TestClient } from './util.js';
+
+test('delete prev failure', async () => {
+	const tc = new TestClient();
+
+	const create = await tc.client.createTestType({text: "foo"});
+	const get = await tc.client.getTestType(create.id);
+	await tc.client.updateTestType(create.id, {text: "bar"});
+
+	assert.rejects(tc.client.deleteTestType(create.id, {prev: get}));
+});
