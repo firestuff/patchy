c59cd3e5558fe47e831191949afc5c3ea6b11b8d
Ian Gulliver <github@flamingcow.io>
2023-04-08 21:13:23 +0000

    Add find*() and find_test

---
+++ api/ts_test/find_test.ts
@@ -0,0 +1,12 @@
+import { test } from 'node:test';
+import { strict as assert } from 'node:assert';
+import { TestClient } from './util.js';
+
+test('find success', async () => {
+	const tc = new TestClient();
+
+	const create = await tc.client.createTestType({text: "foo"});
+
+	const find = await tc.client.findTestType(create.id.substring(0, 4));
+	assert.equal(find.text, "foo");
+});
