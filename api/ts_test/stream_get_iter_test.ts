239ea89e9d1848faf2b155c94a156e89348b198e
Ian Gulliver <github@flamingcow.io>
2023-04-15 04:44:39 +0000

    Implement GetStream<T> AsyncIterableIterator

---
+++ api/ts_test/stream_get_iter_test.ts
@@ -0,0 +1,30 @@
+import { test } from 'node:test';
+import { strict as assert } from 'node:assert';
+import { TestClient } from './util.js';
+
+test('stream get success', async () => {
+	const tc = new TestClient();
+
+	const create = await tc.client.createTestType({text: "foo"});
+
+	const stream = await tc.client.streamGetTestType(create.id);
+
+	await tc.client.updateTestType(create.id, {text: "bar"});
+
+	const evs = [];
+
+	for await (const ev of stream) {
+		evs.push(ev);
+
+		if (evs.length == 2) {
+			break;
+		}
+	}
+
+	assert.equal(evs.length, 2);
+	assert.equal(evs[0]!.obj.text, "foo");
+	assert.equal(evs[1]!.obj.text, "bar");
+
+	stream.abort();
+	assert.rejects(stream.read());
+});
