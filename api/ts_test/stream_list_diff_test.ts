e1b62e89dc121f3ead71f906402abff57d4862cd
Ian Gulliver <github@flamingcow.io>
2023-04-17 17:43:46 +0000

    Local sort for stream list diff

--- api/ts_test/stream_list_diff_test.ts
+++ api/ts_test/stream_list_diff_test.ts
@@ -6,30 +6,27 @@ test('stream list diff success', async () => {
 	const tc = new TestClient();
 
 	await tc.client.createTestType({text: 'foo'});
+	await tc.client.createTestType({text: 'zig'});
+	await tc.client.createTestType({text: 'aaa'});
 
 	const stream = await tc.client.streamListTestType({stream: 'diff', sorts: ['+text']});
 
 	try {
 		const s1 = await stream.read();
 		assert(s1);
-		assert.equal(s1.length, 1);
-		assert.equal(s1[0]!.text, 'foo');
+		assert.deepEqual(s1.map(x => x.text), ['aaa', 'foo', 'zig']);
 
 		const create2 = await tc.client.createTestType({text: 'bar'});
 
 		const s2 = await stream.read();
 		assert(s2);
-		assert.equal(s2.length, 2);
-		assert.equal(s2[0]!.text, 'bar');
-		assert.equal(s2[1]!.text, 'foo');
+		assert.deepEqual(s2.map(x => x.text), ['aaa', 'bar', 'foo', 'zig']);
 
-		await tc.client.updateTestType(create2.id, {text: 'zig'});
+		await tc.client.updateTestType(create2.id, {text: 'zag'});
 
 		const s3 = await stream.read()!;
 		assert(s3);
-		assert.equal(s3.length, 2);
-		assert.equal(s3[0]!.text, 'foo');
-		assert.equal(s3[1]!.text, 'zig');
+		assert.deepEqual(s3.map(x => x.text), ['aaa', 'foo', 'zag', 'zig']);
 	} finally {
 		await stream.close();
 	}
