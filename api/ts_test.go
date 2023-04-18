f18cc14755f5e3dec5a58c7fddc6ddcb2bf1b2c9
Ian Gulliver <github@flamingcow.io>
2023-04-18 16:48:24 +0000

    Split out ts build

--- api/ts_test.go
+++ api/ts_test.go
@@ -12,30 +12,7 @@ import (
 func TestTSNode(t *testing.T) {
 	t.Parallel()
 
-	ta := newTestAPI(t)
-	t.Cleanup(func() {
-		ta.shutdown(t)
-
-		paths, err := filepath.Glob("ts_test/node/*.js")
-		require.NoError(t, err)
-
-		for _, path := range paths {
-			os.Remove(path)
-		}
-
-		os.Remove("ts_test/node/client.ts")
-	})
-
-	ctx := context.Background()
-
-	tc, err := ta.pyc.TSClient(ctx)
-	require.NoError(t, err)
-	require.NotEmpty(t, tc)
-
-	err = os.WriteFile("ts_test/node/client.ts", []byte(tc), 0o600)
-	require.NoError(t, err)
-
-	runNoError(t, "ts_test/node", nil, "tsc", "--pretty")
+	buildTS(t, "ts_test/node")
 
 	paths, err := filepath.Glob("ts_test/node/*_test.js")
 	require.NoError(t, err)
@@ -66,4 +43,32 @@ func testPath(t *testing.T, path string) {
 	runNoError(t, "ts_test/node", env, "node", "--test", "--enable-source-maps", filepath.Base(path))
 
 	require.NoError(t, os.Remove(path))
+}
+
+func buildTS(t *testing.T, path string) {
+	ta := newTestAPI(t)
+
+	t.Cleanup(func() {
+		ta.shutdown(t)
+
+		paths, err := filepath.Glob(filepath.Join(path, "*.js"))
+		require.NoError(t, err)
+
+		for _, path := range paths {
+			os.Remove(path)
+		}
+
+		os.Remove(filepath.Join(path, "client.ts"))
+	})
+
+	ctx := context.Background()
+
+	tc, err := ta.pyc.TSClient(ctx)
+	require.NoError(t, err)
+	require.NotEmpty(t, tc)
+
+	err = os.WriteFile(filepath.Join(path, "client.ts"), []byte(tc), 0o600)
+	require.NoError(t, err)
+
+	runNoError(t, path, nil, "tsc", "--pretty")
 }
